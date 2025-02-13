954a62965073b2c3359831ddb7411c9a8e091b0e
Loravis <loravis@proton.me>
2025-02-13 19:57:06 +0100

    Played around with ags a bit

--- ags/widget/Menu.tsx
+++ ags/widget/Menu.tsx
@@ -4,17 +4,27 @@ import { Variable } from "astal"
 const time = Variable("").poll(1000, "date +\"%H:%M\"") // Retrieve time
 
 export default function Menu(gdkmonitor: Gdk.Monitor) {
-	const { CENTER, RIGHT } = Astal.WindowAnchor
+	const { CENTER, LEFT } = Astal.WindowAnchor
 
 	return <window
 		name="Menu"
 		className="Menu"
 		gdkmonitor={gdkmonitor}
 		exclusivity={Astal.Exclusivity.IGNORE}
-		anchor={CENTER | RIGHT}
+		anchor={CENTER | LEFT}
 		application={App}>
-		<centerbox>
-			<label label={time()} />
-		</centerbox>
+		<box vertical={true}>
+			<centerbox>
+				<label label={time()} />
+			</centerbox>
+			<centerbox>
+				<button hexpand={false} className="leftbtn" onClicked="echo hello">
+					<label label="Hi" />
+				</button>
+				<button hexpand={false} className="rightbtn" onClicked="echo hello">
+					<label label="Hi" />
+				</button>
+			</centerbox>
+		</box>
 	</window>
 }
